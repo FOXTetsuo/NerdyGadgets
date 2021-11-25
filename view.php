790cb158dd37b94d8502c5e5760b5dd363ea103a
FOXTetsuo <pownur@gmail.com>
2021-11-25 13:58:26 +0100

    Winkelmandknop weg als ID niet bestaat

--- view.php
+++ view.php
@@ -146,11 +146,13 @@ if (!empty($_GET['id']))
     }
     ?>
 
+    <?php if (!empty($_GET['id'])){ ?>
     <!-- formulier via POST en niet GET om te zorgen dat refresh van pagina niet het artikel onbedoeld toevoegt-->
     <form method="post">
         <input type="number" name="stockItemID" value="<?php print($stockItemID) ?>" hidden>
         <input type="submit" name="submit" value="Voeg toe aan winkelmandje">
     </form>
+    <?php } ?>
 
     <?php
     if (isset($_POST["submit"])) {              // zelfafhandelend formulier
