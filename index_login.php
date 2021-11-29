1787dae460227f90d05a2212edea9da23e9afba0
FOXTetsuo <pownur@gmail.com>
2021-11-29 13:09:28 +0100

    Update index_login.php

--- index_login.php
+++ index_login.php
@@ -1,37 +1,34 @@
 <?php
 include "header.php";
 ?>
+<!-- Checkt of de "uitloggen" knop is ingedrukt. Zo ja, haalt het wachtwoord en naam van de user weg (niet de user/inlognaam) -->
 <?php
 if (isset($_POST["logout"]))
 {
-    session_destroy();
+    $_SESSION['loggedin']=False;
+    $_SESSION['name']="";
+    $_SESSION['wachtwoord']="";
 }
+// Checkt of de "inloggen" knop ingedrukt is & slaat username en wachtwoord op in de session.
+// Als de username niet leeg is en het wachtwoord het wachtwoord uit de database matcht ("HashedPassword"), word loggedin True
 if (isset($_POST["submit"]))
 {
     $_SESSION['username'] = $_POST["uname"];
     $_SESSION['wachtwoord'] = $_POST["pass"];
-    if((!empty((getPersonID($_SESSION['username'], $databaseConnection))[0]["HashedPassword"])))
+    if((!empty((getPersonID($_SESSION['username'], $databaseConnection))[0]["HashedPassword"])) &&
+        $_SESSION['wachtwoord'] === (getPersonID($_SESSION['username'], $databaseConnection))[0]["HashedPassword"])
         {
-            if ($_SESSION['wachtwoord'] === (getPersonID($_SESSION['username'], $databaseConnection))[0]["HashedPassword"])
-                {
-                    ($_SESSION["loggedin"] = True);
-                }
-            else
-                {
-                    $_SESSION['loggedin']= False;
-                    $_SESSION["name"]="";
-                    $_SESSION['wachtwoord']="";
-                    print("Vul een juiste gebruikersnaam en wachtwoord in");
-                }
+            ($_SESSION["loggedin"] = True);
         }
     else
-    {
-        $_SESSION['loggedin']=False;
-        $_SESSION['name']="";
-        $_SESSION['wachtwoord']="";
-        print("Vul een juiste gebruikersnaam en wachtwoord in");
-    }
+        {
+            $_SESSION['loggedin']=False;
+            $_SESSION['name']="";
+            $_SESSION['wachtwoord']="";
+            print("Vul een juiste gebruikersnaam en wachtwoord in");
+        }
 }
+// Als Loggedin = true, wordt een bericht getoond
 if($_SESSION["loggedin"]===True){
 if(!empty((getPersonID($_SESSION['username'], $databaseConnection))[0]["HashedPassword"]))
 {
@@ -42,7 +39,7 @@ if(!empty((getPersonID($_SESSION['username'], $databaseConnection))[0]["HashedPa
 
 ?>
 <!--Toont login scherm als gebruiker niet al ingelogd is-->
-<?php if ($_SESSION['loggedin']===False){?>
+<?php if ($_SESSION['loggedin']===False || !array_key_exists("loggedin", $_SESSION)){?>
     <form method="post" action="index_login.php">
         <label for="uname">Gebruikersnaam</label><br>
         <input type="text" id="uname" name="uname" class="winkelmandbutton"><br>
