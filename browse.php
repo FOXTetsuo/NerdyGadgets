3cff0fa36e4434ea06752a238940407038b1785b
RowanHuynh <94377432+RowanHuynh@users.noreply.github.com>
2021-11-30 13:40:22 +0100

    voorraad fixen in view
    :)

--- browse.php
+++ browse.php
@@ -1,6 +1,7 @@
 <!-- dit bestand bevat alle code voor het productoverzicht -->
 <?php
 include __DIR__ . "/header.php";
+include "stockfuntions.php";
 
 $ReturnableResult = null;
 $Sort = "SellPrice";
@@ -184,13 +185,7 @@ if (isset($amount)) {
 }
 
 
-    function getVoorraadTekst($actueleVoorraad) {
-        if ($actueleVoorraad > 1000) {
-            return "Ruime voorraad beschikbaar.";
-        } else {
-            return "Voorraad: $actueleVoorraad";
-        }
-    }
+
     function berekenVerkoopPrijs($adviesPrijs, $btw) {
 		return $btw * $adviesPrijs / 100 + $adviesPrijs;
     }
