833cecf7eed18079b73c8f45424ead6da4c63f90
Tysonvb <tysonvanballegooijen@hotmail.com>
2021-12-07 14:11:59 +0100

    Update cart.php

--- cart.php
+++ cart.php
@@ -132,7 +132,7 @@ if (isset($_POST["betalen"]))
 <h5 id="CenteredContent">
 <?php
 
-print("<br> De totale prijs is €". (number_format(round(($totaalprijs), 2),2)));?>
+print("<br> De totale prijs is €". (number_format(round(($totaalprijs), 2),2))." (inc. BTW)");?>
 <?php $_SESSION["totprijs"]=$totaalprijs?>
 </h5>
 <br><br>
