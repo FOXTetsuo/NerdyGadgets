4b5880d979dcd34232436499903b9324d80a704d
FOXTetsuo <60584659+FOXTetsuo@users.noreply.github.com>
2022-01-05 21:50:53 +0100

    overbodige bestanden verwijderd

--- cart.php
+++ cart.php
@@ -48,7 +48,7 @@ if (empty($cart)) {
     </div>
     </body>
 <?php } else {
-// Dit blok code kan ook op een andere pagina geplaatst worden indien gewenst. Is nodig voor betaling, haalt items uit karretje en toont bericht
+// Nodig voor betaling, haalt items uit karretje en toont bericht
     if (isset($_POST["betalen"])) {
         if ($_SESSION["loggedin"] === true) {
             orderItems(getPersonIDNew($_SESSION['username'], $databaseConnection)[0]["USERID"], 1, "SYSTEM", $databaseConnection, 1, $_POST["Voornaam"], $_POST["Achternaam"], $_POST["Straat"], $_POST["Plaats"], $_POST["Postcode"], $_POST["Huisnummer"], $_POST["email"]);
@@ -71,9 +71,6 @@ if (empty($cart)) {
         echo '<script>window.location.href = "betaald.php";</script>';
         exit();
     }
-
-
-// kopieren tot hier :)
     ?>
 
     <h1 id="CenteredContent">Inhoud Winkelwagen</h1>
